e8dae15ce24c1a00948cf2f7de663c983fcd89aa
IkerDV <112340060+IKRDev@users.noreply.github.com>
2022-10-27 11:54:22 -0500

    fix sandboxels mods, and add copy button

--- scripts/funcs.js
+++ scripts/funcs.js
@@ -7,10 +7,12 @@ export function copy(elm) {
   copyText.setSelectionRange(0, 99999); // For mobile devices
 
   // Copy the text inside the text field
-  navigator.clipboard.writeText(copyText.value);
+  navigator.clipboard.writeText(elm.href);
+
+  console.log("copied " + elm.href)
 
   // Alert the copied text
-  alert("Copied the text: " + copyText.value);
+  alert("Copied the text: " + elm.href);
 }
 
 export function getData() {
