2f72d6c0eb0c04fcef9d8fb4e46b293027c39123
IkerDV <112340060+IKRDev@users.noreply.github.com>
2022-10-28 11:52:12 -0500

    Update sbMods.js

--- scripts/sbMods.js
+++ scripts/sbMods.js
@@ -1,3 +1,25 @@
-import {copy, getData} from 'https://ikrdev.github.io/ikerdv/scripts/funcs.js';
+function copy(elm) {
+  // Get the text field
+  var copyText = document.getElementById("mayolink");
 
-window.alert('test');
+  // Select the text field
+  //copyText.select();
+  //copyText.setSelectionRange(0, 99999); // For mobile devices
+
+  // Copy the text inside the text field
+
+  text = document.getElementById(elm).href
+  navigator.clipboard.writeText(text);
+
+  console.log("copied " + text)
+
+  // Alert the copied text
+  alert("Copied the text: " + text);
+}
+
+function getData() {
+  fetch("config.json")
+    .then((response) => response.json())
+    .then((data) => ret = data);
+  return ret;
+}
