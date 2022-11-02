449fdb003035990e70e7975106c4df97c5291bb2
IkerDV <112340060+IKRDev@users.noreply.github.com>
2022-11-02 10:41:52 -0500

    Update boil.js

--- sbmods/boil.js
+++ sbmods/boil.js
@@ -6,6 +6,50 @@ function rand(min, max) {
 	return Math.random() * (max - min) + min;
 }
 
+function doWater() {
+  if (pixel.start === pixelTicks) {
+				return;
+			}
+			if (pixel.charge && elements[pixel.element].behaviorOn) {
+				pixelTick(pixel);
+			}
+			if (elements[pixel.element].viscosity && (!((Math.random() * 100) < 100 / ((elements[pixel.element].viscosity) ** 0.25)))) {
+				var move1Spots = [
+					[pixel.x, pixel.y + 1]
+				];
+			} else {
+				var move1Spots = [
+					[pixel.x + 1, pixel.y + 1],
+					[pixel.x, pixel.y + 1],
+					[pixel.x - 1, pixel.y + 1],
+				]
+			}
+			var moved = false;
+			for (var i = 0; i < move1Spots.length; i++) {
+				var coords = move1Spots[Math.floor(Math.random() * move1Spots.length)];
+				if (tryMove(pixel, coords[0], coords[1])) {
+					moved = true;
+					break;
+				} else {
+					move1Spots.splice(move1Spots.indexOf(coords), 1);
+				}
+			}
+			if (!moved) {
+				if (elements[pixel.element].viscosity === undefined || !(!((Math.random() * 100) < 100 / ((elements[pixel.element].viscosity) ** 0.25)))) {
+					if (Math.random() < 0.5) {
+						if (!tryMove(pixel, pixel.x + 1, pixel.y)) {
+							tryMove(pixel, pixel.x - 1, pixel.y);
+						}
+					} else {
+						if (!tryMove(pixel, pixel.x - 1, pixel.y)) {
+							tryMove(pixel, pixel.x + 1, pixel.y);
+						}
+					}
+				}
+			}
+			doDefaults(pixel);
+		}
+
 elements.wp = {
 	color: ["#ffb48f", "#ffd991", "#ffad91"],
 	behavior: [
@@ -40,61 +84,22 @@ elements.boiling_water = {
 
 elements.boiling_oil = {
 	color: elements.oil.color,
-    tempHigh: 400,
-    stateHigh: "fire",
-    tempLow: 290,
-    stateLow: "oil",
-    reactions: "dirt": { "elem1":null, "elem2":"mud" },
+  tempHigh: 400,
+  stateHigh: "fire",
+  tempLow: 290,
+  stateLow: "oil",
+  reactions: {"dirt": { "elem1":null, "elem2":"mud" },
                 "sand": { "elem1":null, "elem2":"wet_sand" },
-                "dry_ice": {"elem1":null, "elem2":"explosion"},
+              "dry_ice": {"elem1":null, "elem2":"explosion"},},
 	tick: function(pixel) {
 		if (rand(0, 100) >= 50) {
 			//pop
-			place("oil", pixel.x + rand(-1, 2), pixel.y + rand(-1, 2))
-			place("erase", pixel.x, pixel.y)
-		} else {
-			if (pixel.start === pixelTicks) {
-				return
-			}
-			if (pixel.charge && elements[pixel.element].behaviorOn) {
-				pixelTick(pixel)
-			}
-			if (elements[pixel.element].viscosity && (!((Math.random() * 100) < 100 / ((elements[pixel.element].viscosity) ** 0.25)))) {
-				var move1Spots = [
-					[pixel.x, pixel.y + 1]
-				]
-			} else {
-				var move1Spots = [
-					[pixel.x + 1, pixel.y + 1],
-					[pixel.x, pixel.y + 1],
-					[pixel.x - 1, pixel.y + 1],
-				]
-			}
-			var moved = false;
-			for (var i = 0; i < move1Spots.length; i++) {
-				var coords = move1Spots[Math.floor(Math.random() * move1Spots.length)];
-				if (tryMove(pixel, coords[0], coords[1])) {
-					moved = true;
-					break;
-				} else {
-					move1Spots.splice(move1Spots.indexOf(coords), 1);
-				}
-			}
-			if (!moved) {
-				if (elements[pixel.element].viscosity === undefined || !(!((Math.random() * 100) < 100 / ((elements[pixel.element].viscosity) ** 0.25)))) {
-					if (Math.random() < 0.5) {
-						if (!tryMove(pixel, pixel.x + 1, pixel.y)) {
-							tryMove(pixel, pixel.x - 1, pixel.y);
-						}
-					} else {
-						if (!tryMove(pixel, pixel.x - 1, pixel.y)) {
-							tryMove(pixel, pixel.x + 1, pixel.y);
-						}
-					}
-				}
-			}
-			doDefaults(pixel);
+			place("oil", pixel.x + rand(-1, 2), pixel.y + rand(-1, 2));
+			place("erase", pixel.x, pixel.y);
 		}
+    else {
+      doWater();
+    }
 	}
 }
 
